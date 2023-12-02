ba7644a1d70d8671adb16a710c46218b1f235f6d
tanmaytirpankar <tirpankartanmay@gmail.com>
2023-12-02 12:02:46 -0700

    Added support for and tests for LDRB (LDRBBroW and LDRBBroX) clang-formatted using clang-17

--- backend_tv/lifter.cpp
+++ backend_tv/lifter.cpp
@@ -304,37 +304,38 @@ class arm2llvm {
   };
 
   const set<int> instrs_32 = {
-      AArch64::ADDWrx,    AArch64::ADDSWrs,    AArch64::ADDSWri,
-      AArch64::ADDWrs,    AArch64::ADDWri,     AArch64::ADDSWrx,
-      AArch64::ADCWr,     AArch64::ADCSWr,     AArch64::ASRVWr,
-      AArch64::SUBWri,    AArch64::SUBWrs,     AArch64::SUBWrx,
-      AArch64::SUBSWrs,   AArch64::SUBSWri,    AArch64::SUBSWrx,
-      AArch64::SBFMWri,   AArch64::CSELWr,     AArch64::ANDWri,
-      AArch64::ANDWrr,    AArch64::ANDWrs,     AArch64::ANDSWri,
-      AArch64::ANDSWrr,   AArch64::ANDSWrs,    AArch64::MADDWrrr,
-      AArch64::MSUBWrrr,  AArch64::EORWri,     AArch64::CSINVWr,
-      AArch64::CSINCWr,   AArch64::MOVZWi,     AArch64::MOVNWi,
-      AArch64::MOVKWi,    AArch64::LSLVWr,     AArch64::LSRVWr,
-      AArch64::ORNWrs,    AArch64::UBFMWri,    AArch64::BFMWri,
-      AArch64::ORRWrs,    AArch64::ORRWri,     AArch64::SDIVWr,
-      AArch64::UDIVWr,    AArch64::EXTRWrri,   AArch64::EORWrs,
-      AArch64::RORVWr,    AArch64::RBITWr,     AArch64::CLZWr,
-      AArch64::REVWr,     AArch64::CSNEGWr,    AArch64::BICWrs,
-      AArch64::BICSWrs,   AArch64::EONWrs,     AArch64::REV16Wr,
-      AArch64::Bcc,       AArch64::CCMPWr,     AArch64::CCMPWi,
-      AArch64::LDRWui,    AArch64::LDRWroW,    AArch64::LDRWroX,
-      AArch64::LDRSui,    AArch64::LDRBBui,    AArch64::LDRBui,
-      AArch64::LDRSBWui,  AArch64::LDRSWui,    AArch64::LDRSHWui,
-      AArch64::LDRSBWui,  AArch64::LDRHHui,    AArch64::LDRHui,
-      AArch64::STRWui,    AArch64::STRWroW,    AArch64::STRWroX,
-      AArch64::CCMNWi,    AArch64::CCMNWr,     AArch64::STRBBui,
-      AArch64::STRBui,    AArch64::STPWi,      AArch64::STRHHui,
-      AArch64::STRHui,    AArch64::STURWi,     AArch64::STRSui,
-      AArch64::LDPWi,     AArch64::STRWpre,    AArch64::FADDSrr,
-      AArch64::FSUBSrr,   AArch64::FCMPSrr,    AArch64::FCMPSri,
-      AArch64::FMOVSWr,   AArch64::INSvi32gpr, AArch64::INSvi16gpr,
-      AArch64::INSvi8gpr, AArch64::FCVTSHr,    AArch64::FCVTZSUWSr,
-      AArch64::FCSELSrrr, AArch64::FMULSrr,    AArch64::FABSSr,
+      AArch64::ADDWrx,     AArch64::ADDSWrs,    AArch64::ADDSWri,
+      AArch64::ADDWrs,     AArch64::ADDWri,     AArch64::ADDSWrx,
+      AArch64::ADCWr,      AArch64::ADCSWr,     AArch64::ASRVWr,
+      AArch64::SUBWri,     AArch64::SUBWrs,     AArch64::SUBWrx,
+      AArch64::SUBSWrs,    AArch64::SUBSWri,    AArch64::SUBSWrx,
+      AArch64::SBFMWri,    AArch64::CSELWr,     AArch64::ANDWri,
+      AArch64::ANDWrr,     AArch64::ANDWrs,     AArch64::ANDSWri,
+      AArch64::ANDSWrr,    AArch64::ANDSWrs,    AArch64::MADDWrrr,
+      AArch64::MSUBWrrr,   AArch64::EORWri,     AArch64::CSINVWr,
+      AArch64::CSINCWr,    AArch64::MOVZWi,     AArch64::MOVNWi,
+      AArch64::MOVKWi,     AArch64::LSLVWr,     AArch64::LSRVWr,
+      AArch64::ORNWrs,     AArch64::UBFMWri,    AArch64::BFMWri,
+      AArch64::ORRWrs,     AArch64::ORRWri,     AArch64::SDIVWr,
+      AArch64::UDIVWr,     AArch64::EXTRWrri,   AArch64::EORWrs,
+      AArch64::RORVWr,     AArch64::RBITWr,     AArch64::CLZWr,
+      AArch64::REVWr,      AArch64::CSNEGWr,    AArch64::BICWrs,
+      AArch64::BICSWrs,    AArch64::EONWrs,     AArch64::REV16Wr,
+      AArch64::Bcc,        AArch64::CCMPWr,     AArch64::CCMPWi,
+      AArch64::LDRWui,     AArch64::LDRBBroW,   AArch64::LDRBBroX,
+      AArch64::LDRWroW,    AArch64::LDRWroX,    AArch64::LDRSui,
+      AArch64::LDRBBui,    AArch64::LDRBui,     AArch64::LDRSBWui,
+      AArch64::LDRSWui,    AArch64::LDRSHWui,   AArch64::LDRSBWui,
+      AArch64::LDRHHui,    AArch64::LDRHui,     AArch64::STRWui,
+      AArch64::STRWroW,    AArch64::STRWroX,    AArch64::CCMNWi,
+      AArch64::CCMNWr,     AArch64::STRBBui,    AArch64::STRBui,
+      AArch64::STPWi,      AArch64::STRHHui,    AArch64::STRHui,
+      AArch64::STURWi,     AArch64::STRSui,     AArch64::LDPWi,
+      AArch64::STRWpre,    AArch64::FADDSrr,    AArch64::FSUBSrr,
+      AArch64::FCMPSrr,    AArch64::FCMPSri,    AArch64::FMOVSWr,
+      AArch64::INSvi32gpr, AArch64::INSvi16gpr, AArch64::INSvi8gpr,
+      AArch64::FCVTSHr,    AArch64::FCVTZSUWSr, AArch64::FCSELSrrr,
+      AArch64::FMULSrr,    AArch64::FABSSr,
   };
 
   const set<int> instrs_64 = {
@@ -1766,18 +1767,30 @@ public:
 
     int extTyp, shiftAmt;
     switch (CurInst->getOpcode()) {
+    case AArch64::LDRBBroW:
     case AArch64::LDRWroW:
       extTyp = extendTypeVal ? SXTW : UXTW;
+      if (CurInst->getOpcode() == AArch64::LDRBBroW) {
+        assert(shiftAmtVal == 0 &&
+               "AArch64::LDRBBroW shift amount (5th operand) "
+               "is always 0");
+      }
       shiftAmt = shiftAmtVal ? 2 : 0;
       break;
     case AArch64::LDRXroW:
       extTyp = extendTypeVal ? SXTW : UXTW;
       shiftAmt = shiftAmtVal ? 3 : 0;
       break;
+    case AArch64::LDRBBroX:
     case AArch64::LDRWroX:
       // The manual assigns a value LSL to extTyp which for a value of 64
       // bits, is the same as UXTX
       extTyp = extendTypeVal ? SXTX : UXTX;
+      if (CurInst->getOpcode() == AArch64::LDRBBroX) {
+        assert(shiftAmtVal == 0 &&
+               "AArch64::LDRBBroX shift amount (5th operand) "
+               "is always 0");
+      }
       shiftAmt = shiftAmtVal ? 2 : 0;
       break;
     case AArch64::LDRXroX:
@@ -3770,6 +3783,8 @@ public:
       updateReg(added, ptrReg);
       break;
     }
+    case AArch64::LDRBBroW:
+    case AArch64::LDRBBroX:
     case AArch64::LDRWroW:
     case AArch64::LDRWroX:
     case AArch64::LDRXroW:
@@ -3777,6 +3792,10 @@ public:
       unsigned size;
 
       switch (opcode) {
+      case AArch64::LDRBBroW:
+      case AArch64::LDRBBroX:
+        size = 1;
+        break;
       case AArch64::LDRWroX:
       case AArch64::LDRWroW:
         size = 4;
