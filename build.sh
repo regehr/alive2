0f692bbc44e0ebb547e995bf1e87821212352521
rina <k@rina.fyi>
2024-03-04 15:39:06 +1000

    build.sh: install antlr if needed

--- build.sh
+++ build.sh
@@ -2,8 +2,17 @@
 
 set -e -o pipefail
 
+
 export CXXFLAGS
 export CFLAGS
 cd "$(dirname "$0")"
-cmake -B build -DBUILD_TV=1 -DCMAKE_PREFIX_PATH=./antlr-dev/. -DLLVM_DIR=~/progs/llvm-regehr/llvm/build/lib/cmake//llvm/ -DANTLR4_JAR_LOCATION=$(cat antlr-jar) -DFETCHCONTENT_SOURCE_DIR_ASLP-CPP=~/progs/aslp "$@"
+
+if ! [[ -f antlr-jar ]]; then
+  nix build 'nixpkgs#antlr.src' -o antlr-jar
+fi
+if ! [[ -d antlr-dev ]]; then
+  nix build 'nixpkgs#antlr.runtime.cpp^dev' -o antlr
+fi
+
+cmake -B build -DBUILD_TV=1 -DCMAKE_PREFIX_PATH=$(realpath antlr-dev) -DLLVM_DIR=~/progs/llvm-regehr/llvm/build/lib/cmake//llvm/ -DANTLR4_JAR_LOCATION=$(realpath antlr-jar) -DFETCHCONTENT_SOURCE_DIR_ASLP-CPP=~/progs/aslp "$@"
 cmake --build build -j10 -t backend-tv
