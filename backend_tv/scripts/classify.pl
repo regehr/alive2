2297b38b76a973d972e6b4960b7a77ba853b8cbf
rina <k@rina.fyi>
2024-07-04 15:42:13 +1000

    prepare.py

---
+++ backend_tv/scripts/classify.pl
@@ -0,0 +1,152 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+my %results = ();
+my %logs = ();
+my %missing_vec = ();
+
+my $CORRECT = "Translation validated as correct";
+my $UNKNOWN = "[f] Unknown failure";
+
+sub classify($) {
+    (my $ref) = @_;
+    my @lines = @{$ref};
+
+    foreach my $data (@lines) {
+        if ($data =~ /seems to be correct/) {
+            return "[c] $CORRECT";
+        }
+        if ($data =~ /sending signal TERM to command/) {
+            return "[u] Timeout";
+        }
+        if ($data =~ /^(ERROR: volatiles not supported)/) {
+            return "[u] $1";
+        }
+        if ($data =~ /^(ERROR: Out of memory; skipping function.)/) {
+            return "[u] $1";
+        }
+        if ($data =~ /^ERROR: va_arg instructions not supported/) {
+            return "[u] ERROR: varargs not supported";
+        }
+        if ($data =~ /^ERROR: Timeout/) {
+            return "[u] Timeout";
+        }
+        if ($data =~ /^(LLVM ERROR: .*)$/) {
+            return "[f] $1";
+        }
+        if ($data =~ /^(Segmentation fault)/) {
+            return "[f] $1";
+        }
+        if ($data =~ /^(ERROR: Unsupported AArch64 instruction.*)/) {
+            return "[i] $1";
+        }
+        if ($data =~ /^(ERROR: Function is too large)/) {
+            return "[u] $1";
+        }
+        if ($data =~ /^(LLVM ERROR: Cannot select.*)/) {
+            return "[u] $1";
+        }
+        if ($data =~ /^(ERROR: Unsupported metadata.*)/) {
+            return "[u] $1";
+        }
+        if ($data =~ /^(ERROR: Source function is always UB.*)/) {
+            return "[u] $1";
+        }
+        if ($data =~ /^(ERROR: Too many temporaries.*)/) {
+            return "[u] $1";
+        }
+        if ($data =~ /^(ERROR: personality functions not supported.*)/) {
+            return "[u] $1";
+        }
+        if ($data =~ /^(ERROR: NZCV is the only supported case for MSR.*)/) {
+            return "[u] $1";
+        }
+        if ($data =~ /^(ERROR: NZCV is the only supported case for MRS.*)/) {
+            return "[u] $1";
+        }
+        if ($data =~ /^(ERROR: SMT Error.*)/) {
+            return "[u] $1";
+        }
+        if ($data =~ /^(ERROR: Unsupported instruction)/) {
+            return "[u] ERROR: LLVM instruction not supported by Alive2";
+        }
+        if ($data =~ /^(ERROR: The source program doesn't reach a return instruction.*)/) {
+            return "[u] $1";
+        }
+        if ($data =~ /^(ERROR: Unsupported type.*)/) {
+            return "[u] $1";
+        }
+        if ($data =~ /^(ERROR: Only the C calling convention is supported)/) {
+            return "[u] $1";
+        }
+        if ($data =~ /^(ERROR: varargs not supported.*)/) {
+            return "[u] $1";
+        }
+        if ($data =~ /^(ERROR: Unsupported attribute.*)/) {
+            return "[u] $1";
+        }
+        if ($data =~ /^(ERROR: Could not translate.*)/) {
+            return "[u] $1";
+        }
+        if ($data =~ /^(ERROR: Not supporting.*)/) {
+            return "[u] $1";
+        }
+        if ($data =~ /^ERROR: UB triggered/) {
+            return "[f] UB triggered";
+        }
+        if ($data =~ /^ERROR: Value mismatch/) {
+            return "[f] Value mismatch";
+        }
+        if ($data =~ /^(error: .*)$/) {
+            return "[f] $1";
+        }
+        if ($data =~ /^(ERROR: .*)$/) {
+            return "[f] $1";
+        }
+        if ($data =~ /\/(lifter.cpp:[0-9]+):.*Assertion `/) {
+            return "[f] Assertion failed at $1";
+        }
+        if ($data =~ /\/(Instructions.cpp:[0-9]+):.*Assertion `/) {
+            return "[f] Assertion failed at $1";
+        }
+        if ($data =~ /\/(Instructions.cpp:[0-9]+):.*Assertion `/) {
+            return "[f] Assertion failed at $1";
+        }
+        if ($data =~ /\/(MCInst.h:[0-9]+):.*Assertion `/) {
+            return "[f] Assertion failed at $1";
+        }
+        if ($data =~ /^(UNREACHABLE.*)$/) {
+            return "[f] $1";
+        }
+        if ($data =~ /Aslp assertion failure! (.*)$/) {
+            return "[f] Aslp: $1";
+        }
+        if ($data =~ /^[^:]+:\d+:\d+: (runtime error: .*)$/) {
+            return "[f] $1";
+        }
+        if ($data =~ /^SUMMARY: AddressSanitizer: stack-overflow/) {
+            return "[u] stack-overflow";
+        }
+    }
+    return $UNKNOWN;
+}
+
+my %cmds = ();
+my $f = $ARGV[0];
+
+open my $INF, "<$f" or die "reading $f failed";
+my @lines = ();
+my $cmdline;
+while (my $line = <$INF>) {
+    $cmdline = $line unless defined($cmdline);
+    push @lines, $line;
+}
+close $INF;
+my $line = classify(\@lines);
+print "$line\n";
+
+if ($line eq $UNKNOWN) {
+    say STDERR "unknown: $f\n";
+}
---
+++ backend_tv/scripts/prepare.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+
+import os
+import csv
+import sys
+import random
+import subprocess
+import concurrent.futures
+from subprocess import PIPE, DEVNULL
+from collections import defaultdict
+from pathlib import Path
+
+try:
+  from tqdm import tqdm # type: ignore
+except ImportError:
+  def tqdm(it, /, total=None, mininterval=None):
+    if total is None: total = len(it)
+    ndone = 0
+    i = 0
+    split = total // 256
+    for x in it:
+      ndone += 1
+      i += 1
+      if ndone == 1 or i >= split:
+        i = 0
+        print(f'\r{ndone/total:.2%}', end=' ', flush=True, file=sys.stderr)
+      yield x
+
+
+logs_dir = Path(sys.argv[1])
+aslplogs_dir = Path(sys.argv[2])
+assert logs_dir.is_dir()
+assert aslplogs_dir.is_dir()
+
+
+self_dir = Path(os.path.dirname(os.path.realpath(__file__)))
+classify_pl = self_dir / 'classify.pl'
+
+
+def process_log(log: Path, aslplog: Path):
+  proc = subprocess.run([classify_pl, log], stdin=DEVNULL, stdout=PIPE, stderr=PIPE, encoding='utf-8')
+  out, err = map(str.strip, (proc.stdout, proc.stderr))
+  assert not err, f"{log} returned error: {err}"
+
+  proc = subprocess.run([classify_pl, aslplog], stdin=DEVNULL, stdout=PIPE, stderr=PIPE, encoding='utf-8')
+  aout, aerr = map(str.strip, (proc.stdout, proc.stderr))
+  assert not err, f"{aslplog} returned error: {aerr}"
+
+  def simplify_name(s):
+    if s.startswith('classic_'):
+      s = s.replace('classic_', '', 1)
+      out = ''
+      for c in s:
+        if c == c.lower():
+          break
+        out += c
+      return 'classic_' + out
+    return s
+
+  counts = defaultdict(int)
+  with open(log) as f:
+    for l in f:
+      if not l.startswith('encoding counts: '): continue
+      for term in l.replace('encoding counts: ', '').split(','):
+        if '=' not in term: continue
+        k,v = term.split('=')
+        counts[simplify_name(k)] += int(v)
+      break
+
+  return out, aout, counts
+
+def main():
+  logs = list(logs_dir.glob('*.log'))
+  for f in logs:
+    assert (aslplogs_dir / f.name).exists()
+  random.shuffle(logs)
+
+  classified: dict[Path, tuple[str, str, dict]] = {}
+  with concurrent.futures.ThreadPoolExecutor() as pool:
+    futures = {pool.submit(process_log, f, aslplogs_dir / f.name): f for f in logs}
+    for f in tqdm(concurrent.futures.as_completed(futures.keys()), total=len(futures), mininterval=1):
+      classified[futures[f]] = f.result()
+
+
+  keys = ['id']
+  keys += sorted(set(k for _,_,x in classified.values() for k in x.keys()))
+  keys += ['old_outcome']
+  keys += ['old_detail']
+  keys += ['aslp_outcome']
+  keys += ['aslp_detail']
+
+  with open('out.csv', 'w', newline='') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=keys, restval=0)
+    writer.writeheader()
+
+    for f, (classic, aslp, counts) in tqdm(classified.items()):
+      t = lambda x: x.split(' ')[0]
+      row = {'id': f.stem, 'old_outcome': t(classic), 'old_detail': classic, 'aslp_outcome': t(aslp), 'aslp_detail': aslp}
+      row |= counts
+      assert set(row.keys()) <= set(keys)
+      writer.writerow(row) # type: ignore
+
+if __name__ == '__main__':
+  main()
