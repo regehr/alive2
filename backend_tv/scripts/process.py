0d8cb24aaa1b58d42a90decc355749ca862aaa8d
rina <k@rina.fyi>
2024-07-09 15:13:35 +1000

    show relative diff between reg and non-reg

--- backend_tv/scripts/process.py
+++ backend_tv/scripts/process.py
@@ -8,7 +8,7 @@ csv_file = sys.argv[1]
 def main():
   df = pd.read_csv(csv_file, index_col=0)
 
-  # drop instruction name for unsupported instructions to combine rows
+  # drop instruction name for unsupported instructions to reduce number of distinct rows
   df["old_detail"] = df["old_detail"].apply(
     lambda x: x if 'Unsupported AArch64 instruction:' not in x else ':'.join(x.split(':')[:2])
   )
@@ -18,21 +18,38 @@ def main():
 
   # tabulates changes
   print(changed[['old_detail', 'aslp_detail']].value_counts().to_string())
+  print()
+  print(changed[['old_outcome', 'aslp_outcome']].value_counts().to_string())
 
   print()
   print()
   # print(df.loc[df.old_detail.str.startswith('[i] ERROR: Unsu')])
 
-  # total counts of each category
+  # total counts of each [c] [u] [i] [f] category
   print()
   print(df[['old_outcome']].value_counts())
   print()
   print(df[['aslp_outcome']].value_counts())
 
-  regressed = df.loc[(df['old_outcome'] == '[c]') & (df['aslp_outcome'] != '[c]')]
+  # XXX: assume numeric columns are encoding counts
   num_cols = df.select_dtypes(np.number).columns
-  # for each encoding, computes the proportion of regressed tests in which it appears
-  print(regressed[num_cols].clip(0,1).sum().div(len(regressed)).sort_values().to_string())
+
+  regressed = df.loc[(df['old_outcome'] == '[c]') & (df['aslp_outcome'] != '[c]')]
+  nonregressed = df.loc[(df['old_outcome'] == '[c]') & (df['aslp_outcome'] == '[c]')]
+
+  # for each encoding, computes the proportion of (non)regressed tests in which it appears
+  reg = regressed[num_cols].clip(0,1).sum().div(len(regressed))
+  nonreg = nonregressed[num_cols].clip(0,1).sum().div(len(nonregressed))
+
+  # print(reg.sort_values())
+  # print(nonreg.sort_values())
+
+  # identify encodings likely to cause timeout by 
+  rel = pd.DataFrame({'regressed':reg, 'nonregressed':nonreg})
+  rel = rel.loc[(rel['regressed'] != 0) | (rel['nonregressed'] != 0)]
+  rel['relative'] = rel['regressed'] / rel['nonregressed']
+  rel.sort_values('relative', inplace=True)
+  print(rel.tail(10).to_string())
 
 if __name__ == '__main__':
   main()
