ecb56f3998fa05eecd99a624e955e38440fa3b0a
John Regehr <john.regehr@gmail.com>
2025-07-07 08:47:51 -0600

    turn riscv checks back on

--- backend_tv/riscv2llvm.cpp
+++ backend_tv/riscv2llvm.cpp
@@ -373,7 +373,6 @@ Value *riscv2llvm::getPointerOperand() {
 void riscv2llvm::checkArgSupport(Argument &arg) {}
 void riscv2llvm::checkFuncSupport(Function &func) {}
 void riscv2llvm::checkTypeSupport(Type *ty) {
-#if 0
   if (ty->isFloatingPointTy()) {
     *out << "\nERROR: floating point not yet supported\n\n";
     exit(-1);
@@ -382,5 +381,4 @@ void riscv2llvm::checkTypeSupport(Type *ty) {
     *out << "\nERROR: vectors not yet supported\n\n";
     exit(-1);
   }
-#endif
 }
