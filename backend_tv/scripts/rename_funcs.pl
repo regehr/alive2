d15c1026b25823a3a3f38a27860af35be419d727
John Regehr <regehr@cs.utah.edu>
2025-08-14 13:00:10 -0600

    oops missed matching dots

--- backend_tv/scripts/rename_funcs.pl
+++ backend_tv/scripts/rename_funcs.pl
@@ -4,7 +4,7 @@ use warnings;
 use strict;
 
 while (my $line = <STDIN>) {
-    if ($line =~ /^define (.+) \@([0-9a-zA-Z\_]+)\((.*)$/) {
+    if ($line =~ /^define (.+) \@([\.0-9a-zA-Z\_]+)\((.*)$/) {
 	print "define $1 \@f($3\n";
     } else {
 	print $line;
